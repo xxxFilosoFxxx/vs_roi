c39804736f51781b4c2ba7d101d9ac9cb534f3f5
xxxFilosoFxxx <roman.lashnev@mail.ru>
2020-10-29 01:27:56 +0300

    add 03

---
+++ 03/src/header.h
@@ -0,0 +1,167 @@
+#pragma once
+#include <ctime>
+#include <string>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <list>
+#include <chrono>
+
+#include <QCoreApplication>
+#include <QtConcurrent>
+#include <QStringList>
+#include <QTextCodec>
+#include <QString>
+
+namespace head {
+	struct Names_average {
+		std::map<std::string, int> average_salary_humans;
+		std::list<int> list_average_salary_humans;
+
+		Names_average() = default;
+
+		Names_average(std::map<std::string, int> average_salary_humans,
+					  std::list<int> list_average_salary_humans) {
+			this->average_salary_humans = average_salary_humans;
+			this->list_average_salary_humans = list_average_salary_humans;
+		}
+	};
+
+	struct Value_average {
+		std::vector<std::string> names;
+		std::vector<std::vector<int>> vector;
+		size_t first;
+		size_t last;
+
+		Value_average() = default;
+
+		Value_average(std::vector<std::string> names,
+					  std::vector<std::vector<int>> vector,
+					  size_t first,
+					  size_t last) {
+			this->names = names;
+			this->vector = vector;
+			this->first = first;
+			this->last = last;
+		}
+	};
+
+	// Запись имен нужных файлов
+	void writeNames(std::vector<std::string> &text_names,
+					std::vector<std::string> &names,
+					std::string              fileInputName,
+					std::ifstream            &list) {
+
+		while (!list.eof())
+		{
+			std::string list_names;
+			list >> list_names;
+			std::string one_name = list_names;
+			one_name.resize(fileInputName.size());
+			if (one_name == fileInputName) {
+				text_names.push_back(list_names);
+				names.push_back(list_names.substr(0, list_names.length() - 4));
+			}
+		}
+	}
+
+	// Открытие каждого текстового файла для нахождения и запись каждой числа в вектор значений
+	// В дальнейшем вектора заходят в очередь векторов
+	std::vector<std::vector<int>> opendAndRead(std::vector<std::string> &text_names) {
+
+		std::vector<std::vector<int>> vector;
+
+		for (auto& _names : text_names) {
+			std::ifstream fin(_names, std::ios::in);
+			std::vector<int> vec;
+			for (int i = 0; i < 12; i++) {
+				int num;
+				fin >> num;
+				vec.push_back(num);
+			}
+			vector.push_back(vec);
+			fin.close();
+		}
+
+		return vector;
+	}
+
+	// Высчитвание средней зп и запись в map для вывода в .csv файл
+	Names_average mapAverage(const Value_average &vectorAverage) {
+
+		std::map<std::string, int> average_salary_humans;
+		std::list<int> list_average_salary_humans;
+
+		for (auto i = vectorAverage.first; i < vectorAverage.last; ++i) {
+			int average_salary = 0;
+			std::vector<int> vec = vectorAverage.vector[i];
+			for (int j = 0; j < 12; j++) {
+				int num = vec.at(j);
+				average_salary += num;
+			}
+
+			average_salary = average_salary / 12;
+			average_salary_humans[vectorAverage.names.at(i)] = average_salary;
+			list_average_salary_humans.push_back(average_salary);
+		}
+
+		Names_average namesAverage = Names_average(average_salary_humans, list_average_salary_humans);
+		return namesAverage;
+	}
+
+	void reduceAverage(Names_average &n1, const Names_average &n2) {
+		// Объединение map
+		std::map<std::string, int> val = n1.average_salary_humans;
+		val.insert(n2.average_salary_humans.begin(), n2.average_salary_humans.end());
+		n1.average_salary_humans = val;
+
+		// Объединение list
+		n1.list_average_salary_humans.insert(n1.list_average_salary_humans.end(), n2.list_average_salary_humans.begin(), n2.list_average_salary_humans.end());
+	}
+
+	// 5% на max и min зп
+	void percent(std::list<int> list_average_salary_humans,
+				 std::map<std::string, int> &average_salary_humans,
+				 std::list<std::string> &human_low_salary,
+				 std::list<std::string> &human_high_salary) {
+
+		size_t size = list_average_salary_humans.size();
+		size = size * 0.05;
+
+		std::list<int>         low_salary;
+		std::list<int>         high_salary;
+
+		list_average_salary_humans.sort();
+		low_salary = list_average_salary_humans;
+		low_salary.resize(size);
+
+		list_average_salary_humans.reverse();
+		high_salary = list_average_salary_humans;
+		high_salary.resize(size);
+
+		for (auto & average_salary_human : average_salary_humans) {
+			for (int n : low_salary) {
+				if (average_salary_human.second == n)
+					human_low_salary.push_back(average_salary_human.first);
+			}
+
+			for (int n : high_salary) {
+				if (average_salary_human.second == n)
+					human_high_salary.push_back(average_salary_human.first);
+			}
+		}
+	}
+
+	// зп ниже среднего
+	void below_the_set_value(std::string param,
+							 std::list<int> &list_average_salary_humans,
+							 int &count_for_param) {
+
+		int int_param = std::stoi(param);
+
+		for (int n : list_average_salary_humans) {
+			if (n < int_param)
+				count_for_param++;
+		}
+	}
+}
--- 03/src/main.cpp
+++ 03/src/main.cpp
@@ -1,23 +1,132 @@
-#include <QCoreApplication>
-#include <QtConcurrent>
-#include <QStringList>
-#include <QTextCodec>
-#include <QString>
-
-#include <ctime>
-#include <vector>
 #include <iostream>
+#include "header.h"
 
-int main ( int argc_p, char ** argv_p ) { 
-   QCoreApplication app_l( argc_p, argv_p );
-   static const auto * const codel_scl = QTextCodec::codecForName( "IBM 866" );
-   std::cout << "\n ---------------------------------\n";
-   QStringList argumentsList_l;
-   const std::size_t argumetsAmount_cl = argc_p;
-   for ( std::size_t i = 0; i < argumetsAmount_cl; ++i )
-      argumentsList_l.push_back( codel_scl->toUnicode( argv_p[ i ] ) );
-   for ( auto oneArg : argumentsList_l )
-      std::cout << "\t" << codel_scl->fromUnicode( oneArg ).constData() << "\n";
-   std::cout << " ---------------------------------";
-   return 0;
+int main(int argc_p, char **argv_p) {
+	using namespace head;
+
+	QCoreApplication app_l(argc_p, argv_p);
+	static const auto * const codel_scl = QTextCodec::codecForName("IBM 866");
+	std::cout << "\n ---------------------------------\n";
+	QStringList argumentsList_l;
+	const std::size_t argumetsAmount_cl = argc_p;
+	for (std::size_t i = 0; i < argumetsAmount_cl; ++i)
+		argumentsList_l.push_back(codel_scl->toUnicode(argv_p[i]));
+	for (auto oneArg : argumentsList_l)
+		std::cout << "\t" << codel_scl->fromUnicode(oneArg).constData() << "\n";
+	std::cout << " ---------------------------------\n";
+
+	if (std::string(argv_p[1]) != "--input" || std::string(argv_p[3]) != "--output" || std::string(argv_p[5]) != "--param") {
+		std::cerr << "Invalid command line arguments" << std::endl;
+		return -1;
+	}
+
+	// Для общей части имени входных файлов
+	if (std::string(argv_p[2]).empty() || std::string(argv_p[4]).empty() || std::string(argv_p[6]).empty()) {
+		std::cerr << "Invalid command line arguments" << std::endl;
+		return -1;
+	}
+
+	// Название .csv файла
+	std::string fileCfgName = argv_p[4];
+	std::string name_csv = fileCfgName.substr(fileCfgName.length() - 4, 4);
+
+	if (name_csv != ".csv") {
+		std::cerr << "Invalid file *.csv" << std::endl;
+		return -1;
+	}
+
+	// Запись всех текстовых файлов
+	std::system("dir /b /o:d *.txt > list.txt");
+	std::vector<std::string>	  text_names;
+	std::vector<std::string>	  names;
+	std::list<std::string>		  human_low_salary;
+	std::list<std::string>		  human_high_salary;
+	std::vector<std::vector<int>> vector;
+	int below_average = 0;
+
+	std::string fileInputName = argv_p[2];
+	std::string param = argv_p[6];
+
+	std::ifstream list("list.txt", std::ios::in);
+	if (!list.is_open()) {
+		std::cerr << "*.txt files not found" << std::endl;
+		return -1;
+	}
+
+	writeNames(text_names, names, fileInputName, list);
+	vector = opendAndRead(text_names);
+
+	// Максимальное кол-во оптимально открытых потоков и разделение размерности вектора
+	const auto Current_Hardware = std::thread::hardware_concurrency();
+	size_t vecSize = 0;
+	if (vector.size() % Current_Hardware == 0) {
+		vecSize = vector.size() / Current_Hardware;
+	}
+	else {
+		vecSize = (vector.size() / Current_Hardware) + 1;
+	}
+
+	size_t first = 0;
+	size_t last = vecSize;
+
+	//using Tasks = std::vector<Value_average>;
+	std::vector<Value_average> tasks;
+
+	for (unsigned int i = 0; i < Current_Hardware; ++i) {
+		tasks.push_back(Value_average(names, vector, first, last));
+		first += vecSize;
+		last = std::min(last + vecSize, vector.size());
+	}
+
+	auto start1 = std::chrono::steady_clock::now();
+
+	Names_average namesAverage = QtConcurrent::blockingMappedReduced(tasks, mapAverage, reduceAverage);
+
+	auto end1 = std::chrono::steady_clock::now();
+	std::chrono::duration<double, std::milli> elapsed_seconds = end1 - start1;
+
+	std::thread t1 = std::thread(percent, namesAverage.list_average_salary_humans, std::ref(namesAverage.average_salary_humans), std::ref(human_low_salary), std::ref(human_high_salary));
+	std::thread t2 = std::thread(below_the_set_value, param, std::ref(namesAverage.list_average_salary_humans), std::ref(below_average));
+
+	auto start2 = std::chrono::steady_clock::now();
+
+	t1.join();
+	t2.join();
+
+	auto end2 = std::chrono::steady_clock::now();
+	elapsed_seconds += end2 - start2;
+
+	// Запись времени работы программы в ms
+	std::ofstream fout_time("time.txt", std::ios::out);
+	fout_time << "Количество файлов: " << names.size() << std::endl << "Время работы программы: " << elapsed_seconds.count() << "ms";
+	fout_time.close();
+
+	// Работа с csv файлом
+	std::string fileOutputName = argv_p[4];
+	std::ofstream fout(fileOutputName, std::ios::out);
+
+	fout << "Гражданин, Среднемесячная зарплата" << "\n";
+	for (auto & average_salary_human : namesAverage.average_salary_humans) {
+		fout << average_salary_human.first << ", " << average_salary_human.second << "\n";
+	}
+
+	fout << "\n";
+	fout << "5% граждан с max зп, 5% граждан с min зп" << "\n";
+
+	auto it1 = human_high_salary.begin();
+	auto it2 = human_low_salary.begin();
+
+	while (it1 != human_high_salary.end() && it2 != human_low_salary.end()) {
+		fout << *it1 << ", " << *it2 << "\n";
+		++it1;
+		++it2;
+	}
+	fout << "\n";
+
+	fout << "Количество граждан с зп ниже среднего" << "\n";
+	fout << below_average << "\n";
+
+	fout.close();
+
+	return 0;
 }
